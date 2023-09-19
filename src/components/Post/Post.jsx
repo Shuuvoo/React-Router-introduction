b7e32360b22194b6144f153095250ac0eb81ae55
Shuuvoo <shekhoshuvo@gmail.com>
2023-09-19 21:07:30 +0600

    navigate  added with go back button added

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const Post = ({post}) => {
     const {id,title} = post
+    const navigate = useNavigate()
     const postStyle = {
         padding: '10px',
         border: '1px solid pink',
         borderRadius: '15px',
 
 
+    }
+    const handleShowdeatail = () => {
+        navigate(`/post/${id}/`)
     }
 
     return (
@@ -17,6 +21,7 @@ const Post = ({post}) => {
             <p>{title}</p>
             <Link to={`/post/${id}`}>Post Deatails</Link>
             <Link to={`/post/${id}`}><button>Show details</button></Link>
+            <button onClick={handleShowdeatail} >Click to show Details</button>
         </div>
     );
 };
